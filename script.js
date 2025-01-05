9676af27ba28a69b1b6057dd8e1cbc18c80ac8e6
S3bsval <112829014+S3bsval@users.noreply.github.com>
2025-01-04 20:20:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,132 +1,355 @@
-'use strict';
+import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.18.2/+esm"
 
-//Opening or closing side bar
+const canvasEl = document.querySelector("canvas");
+const textureEl = document.createElement("canvas");
+const textureCtx = textureEl.getContext("2d");
 
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-sidebarBtn.addEventListener("click", function() {elementToggleFunc(sidebar); })
-
-//Activating Modal-testimonial
-
-const testimonialsItem = document.querySelectorAll('[data-testimonials-item]');
-const modalContainer = document.querySelector('[data-modal-container]');
-const modalCloseBtn = document.querySelector('[data-modal-close-btn]');
-const overlay = document.querySelector('[data-overlay]');
-
-const modalImg = document.querySelector('[data-modal-img]');
-const modalTitle = document.querySelector('[data-modal-title]');
-const modalText = document.querySelector('[data-modal-text]');
-
-const testimonialsModalFunc = function () {
-    modalContainer.classList.toggle('active');
-    overlay.classList.toggle('active');
+const fontOptions = {
+    "Arial": "Arial, sans-serif",
+    "Verdana": "Verdana, sans-serif",
+    "Tahoma": "Tahoma, sans-serif",
+    "Times New Roman": "Times New Roman, serif",
+    "Georgia": "Georgia, serif",
+    "Garamond": "Garamond, serif",
+    "Courier New": "Courier New, monospace",
+    "Brush Script MT": "Brush Script MT, cursive"
 }
 
-for (let i = 0; i < testimonialsItem.length; i++) {
-    testimonialsItem[i].addEventListener('click', function () {
-        modalImg.src = this.querySelector('[data-testimonials-avatar]').src;
-        modalImg.alt = this.querySelector('[data-testimonials-avatar]').alt;
-        modalTitle.innerHTML = this.querySelector('[data-testimonials-title]').innerHTML;
-        modalText.innerHTML = this.querySelector('[data-testimonials-text]').innerHTML;
+const params = {
+    fontName: "Verdana",
+    isBold: false,
+    fontSize: 80,
+    text: "fluid",
+    pointerSize: null,
+    color: {r: 1., g: .0, b: .5}
+};
 
-        testimonialsModalFunc();
-    })
+const pointer = {
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0,
+    moved: false,
+};
+
+
+let outputColor, velocity, divergence, pressure, canvasTexture;
+let isPreview = true;
+
+const gl = canvasEl.getContext("webgl");
+gl.getExtension("OES_texture_float");
+
+const vertexShader = createShader(
+    document.getElementById("vertShader").innerHTML,
+    gl.VERTEX_SHADER);
+
+const splatProgram = createProgram("fragShaderPoint");
+const divergenceProgram = createProgram("fragShaderDivergence");
+const pressureProgram = createProgram("fragShaderPressure");
+const gradientSubtractProgram = createProgram("fragShaderGradientSubtract");
+const advectionProgram = createProgram("fragShaderAdvection");
+const outputShaderProgram = createProgram("fragShaderOutputShader");
+
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1, -1,
+    -1, 1,
+    1, 1,
+    1, -1
+]), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
+gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(0);
+
+
+createTextCanvasTexture();
+initFBOs();
+createControls();
+setupEvents();
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+render();
+
+function createTextCanvasTexture() {
+    canvasTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 }
 
-//Activating close button in modal-testimonial
+function updateTextCanvas() {
+    textureCtx.fillStyle = "black";
+    textureCtx.fillRect(0, 0, textureEl.width, textureEl.height);
 
-modalCloseBtn.addEventListener('click', testimonialsModalFunc);
-overlay.addEventListener('click', testimonialsModalFunc);
+    textureCtx.font = (params.isBold ? "bold" : "normal") + " " + (params.fontSize * devicePixelRatio) + "px " + fontOptions[params.fontName];
+    textureCtx.fillStyle = "#ffffff";
+    textureCtx.textAlign = "center";
 
-//Activating Filter Select and filtering options
+    textureCtx.filter = "blur(3px)";
 
-const select = document.querySelector('[data-select]');
-const selectItems = document.querySelectorAll('[data-select-item]');
-const selectValue = document.querySelector('[data-select-value]');
-const filterBtn = document.querySelectorAll('[data-filter-btn]');
+    const textBox = textureCtx.measureText(params.text);
+    textureCtx.fillText(params.text, .5 * textureEl.width, .5 * textureEl.height + .5 * textBox.actualBoundingBoxAscent);
 
-select.addEventListener('click', function () {elementToggleFunc(this); });
-
-for(let i = 0; i < selectItems.length; i++) {
-    selectItems[i].addEventListener('click', function() {
-
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        elementToggleFunc(select);
-        filterFunc(selectedValue);
-
-    });
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureEl);
 }
 
-const filterItems = document.querySelectorAll('[data-filter-item]');
+function createProgram(elId) {
+    const shader = createShader(
+        document.getElementById(elId).innerHTML,
+        gl.FRAGMENT_SHADER);
+    const program = createShaderProgram(vertexShader, shader);
+    const uniforms = getUniforms(program);
+    return {
+        program, uniforms
+    }
+}
 
-const filterFunc = function (selectedValue) {
-    for(let i = 0; i < filterItems.length; i++) {
-        if(selectedValue == "all") {
-            filterItems[i].classList.add('active');
-        } else if (selectedValue == filterItems[i].dataset.category) {
-            filterItems[i].classList.add('active');
-        } else {
-            filterItems[i].classList.remove('active');
+function createShaderProgram(vertexShader, fragmentShader) {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error("Unable to initialize the shader program: " + gl.getProgramInfoLog(program));
+        return null;
+    }
+
+    return program;
+}
+
+function getUniforms(program) {
+    let uniforms = [];
+    let uniformCount = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+    for (let i = 0; i < uniformCount; i++) {
+        let uniformName = gl.getActiveUniform(program, i).name;
+        uniforms[uniformName] = gl.getUniformLocation(program, uniformName);
+    }
+    return uniforms;
+}
+
+function createShader(sourceCode, type) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, sourceCode);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+function blit(target) {
+    if (target == null) {
+        gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    } else {
+        gl.viewport(0, 0, target.width, target.height);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, target.fbo);
+    }
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+}
+
+function initFBOs() {
+    const fboSize = [
+        Math.floor(.5 * window.innerWidth),
+        Math.floor(.5 * window.innerHeight),
+    ]
+    outputColor = createDoubleFBO(fboSize[0], fboSize[1]);
+    velocity = createDoubleFBO(fboSize[0], fboSize[1], gl.RG);
+    divergence = createFBO(fboSize[0], fboSize[1], gl.RGB);
+    pressure = createDoubleFBO(fboSize[0], fboSize[1], gl.RGB);
+}
+
+
+function createFBO(w, h, type = gl.RGBA) {
+    gl.activeTexture(gl.TEXTURE0);
+
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, type, w, h, 0, type, gl.FLOAT, null);
+
+    const fbo = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.viewport(0, 0, w, h);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    return {
+        fbo,
+        width: w,
+        height: h,
+        attach(id) {
+            gl.activeTexture(gl.TEXTURE0 + id);
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            return id;
+        }
+    };
+}
+
+function createDoubleFBO(w, h, type) {
+    let fbo1 = createFBO(w, h, type);
+    let fbo2 = createFBO(w, h, type);
+
+    return {
+        width: w,
+        height: h,
+        texelSizeX: 1. / w,
+        texelSizeY: 1. / h,
+        read: () => {
+            return fbo1;
+        },
+        write: () => {
+            return fbo2;
+        },
+        swap() {
+            let temp = fbo1;
+            fbo1 = fbo2;
+            fbo2 = temp;
         }
     }
 }
 
-//Enabling filter button for larger screens 
+function render(t) {
 
-let lastClickedBtn = filterBtn[0];
+    const dt = 1 / 60;
 
-for (let i = 0; i < filterBtn.length; i++) {
-    
-    filterBtn[i].addEventListener('click', function() {
+    if (t && isPreview) {
+        updateMousePosition(
+            (.5 - .45 * Math.sin(.003 * t - 2)) * window.innerWidth,
+            (.5 + .1 * Math.sin(.0025 * t) + .1 * Math.cos(.002 * t)) * window.innerHeight
+        );
+    }
 
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        filterFunc(selectedValue);
+    if (pointer.moved) {
+        if (!isPreview) {
+            pointer.moved = false;
+        }
 
-        lastClickedBtn.classList.remove('active');
-        this.classList.add('active');
-        lastClickedBtn = this;
+        gl.useProgram(splatProgram.program);
+        gl.uniform1i(splatProgram.uniforms.u_input_texture, velocity.read().attach(1));
+        gl.uniform1f(splatProgram.uniforms.u_ratio, canvasEl.width / canvasEl.height);
+        gl.uniform2f(splatProgram.uniforms.u_point, pointer.x / canvasEl.width, 1 - pointer.y / canvasEl.height);
+        gl.uniform3f(splatProgram.uniforms.u_point_value, pointer.dx, -pointer.dy, 1);
+        gl.uniform1f(splatProgram.uniforms.u_point_size, params.pointerSize);
+        blit(velocity.write());
+        velocity.swap();
 
-    })
+        gl.uniform1i(splatProgram.uniforms.u_input_texture, outputColor.read().attach(1));
+        gl.uniform3f(splatProgram.uniforms.u_point_value, 1. - params.color.r, 1. - params.color.g, 1. - params.color.b);
+        blit(outputColor.write());
+        outputColor.swap();
+    }
+
+    gl.useProgram(divergenceProgram.program);
+    gl.uniform2f(divergenceProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
+    gl.uniform1i(divergenceProgram.uniforms.u_velocity_texture, velocity.read().attach(1));
+    blit(divergence);
+
+    gl.useProgram(pressureProgram.program);
+    gl.uniform2f(pressureProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
+    gl.uniform1i(pressureProgram.uniforms.u_divergence_texture, divergence.attach(1));
+
+    for (let i = 0; i < 10; i++) {
+        gl.uniform1i(pressureProgram.uniforms.u_pressure_texture, pressure.read().attach(2));
+        blit(pressure.write());
+        pressure.swap();
+    }
+
+    gl.useProgram(gradientSubtractProgram.program);
+    gl.uniform2f(gradientSubtractProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
+    gl.uniform1i(gradientSubtractProgram.uniforms.u_pressure_texture, pressure.read().attach(1));
+    gl.uniform1i(gradientSubtractProgram.uniforms.u_velocity_texture, velocity.read().attach(2));
+    blit(velocity.write());
+    velocity.swap();
+
+    gl.useProgram(advectionProgram.program);
+    gl.uniform1f(advectionProgram.uniforms.u_use_text, 0);
+    gl.uniform2f(advectionProgram.uniforms.u_texel, velocity.texelSizeX, velocity.texelSizeY);
+    gl.uniform1i(advectionProgram.uniforms.u_velocity_texture, velocity.read().attach(1));
+    gl.uniform1i(advectionProgram.uniforms.u_input_texture, velocity.read().attach(1));
+    gl.uniform1f(advectionProgram.uniforms.u_dt, dt);
+    blit(velocity.write());
+    velocity.swap();
+
+    gl.useProgram(advectionProgram.program);
+    gl.uniform1f(advectionProgram.uniforms.u_use_text, 1);
+    gl.uniform2f(advectionProgram.uniforms.u_texel, outputColor.texelSizeX, outputColor.texelSizeY);
+    gl.uniform1i(advectionProgram.uniforms.u_input_texture, outputColor.read().attach(2));
+    blit(outputColor.write());
+    outputColor.swap();
+
+    gl.useProgram(outputShaderProgram.program);
+    gl.uniform1i(outputShaderProgram.uniforms.u_output_texture, outputColor.read().attach(1));
+
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    requestAnimationFrame(render);
 }
 
-// Enabling Contact Form
-
-const form = document.querySelector('[data-form]');
-const formInputs = document.querySelectorAll('[data-form-input]');
-const formBtn = document.querySelector('[data-form-btn]');
-
-for(let i = 0; i < formInputs.length; i++) {
-    formInputs[i].addEventListener('input', function () {
-        if(form.checkValidity()) {
-            formBtn.removeAttribute('disabled');
-        } else { 
-            formBtn.setAttribute('disabled', '');
-        }
-    })
+function resizeCanvas() {
+    params.pointerSize = 4 / window.innerHeight;
+    canvasEl.width = textureEl.width = window.innerWidth;
+    canvasEl.height = textureEl.height = window.innerHeight;
+	 initFBOs();
+    updateTextCanvas();
 }
 
-// Enabling Page Navigation 
-
-const navigationLinks = document.querySelectorAll('[data-nav-link]');
-const pages = document.querySelectorAll('[data-page]');
-
-for(let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener('click', function() {
-        
-        for(let i = 0; i < pages.length; i++) {
-            if(this.innerHTML.toLowerCase() == pages[i].dataset.page) {
-                pages[i].classList.add('active');
-                navigationLinks[i].classList.add('active');
-                window.scrollTo(0, 0);
-            } else {
-                pages[i].classList.remove('active');
-                navigationLinks[i]. classList.remove('active');
-            }
-        }
+function setupEvents() {
+    canvasEl.addEventListener("mousemove", (e) => {
+        isPreview = false;
+        updateMousePosition(e.pageX, e.pageY);
     });
+
+    canvasEl.addEventListener("touchmove", (e) => {
+        e.preventDefault();
+        isPreview = false;
+        updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+    });
+}
+
+function updateMousePosition(eX, eY) {
+    pointer.moved = true;
+    pointer.dx = 5 * (eX - pointer.x);
+    pointer.dy = 5 * (eY - pointer.y);
+    pointer.x = eX;
+    pointer.y = eY;
+}
+
+function createControls() {
+    const gui = new GUI();
+	 gui.close();
+	
+    gui
+        .add(params, "text")
+        .onChange(updateTextCanvas);
+    gui
+        .add(params, "fontSize", 10, 300)
+        .onChange(updateTextCanvas)
+        .name("font size, px");
+    gui
+        .add(params, "isBold")
+        .onChange(updateTextCanvas)
+        .name("bold");
+    gui
+        .add(params, "fontName", Object.keys(fontOptions))
+        .onChange(updateTextCanvas)
+        .name("font");
+    gui
+        .addColor(params, "color");
 }
